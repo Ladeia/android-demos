49785cebdaf7bbc62b778ac52f1e8665495ef4ca
Alexandre Amoedo Amorim <alexandre.amorim@consultores.reconcavo.org.br>
2016-06-29 17:24:47 -0300

    Added FilePicker code

--- app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
+++ app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.whatever.test.androiddemos;
 
+import android.content.Intent;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pickFile(View view){
+
+        Intent intent = new Intent();
+
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        TextView tv = (TextView) findViewById(R.id.msg_text_view);
+
+        tv.setText(data.getData().toString());
     }
 }
