0a115a28d7b905edc60b0d61d9bcd31c12786e2a
Alexandre Amoedo Amorim <alexandre.amorim@consultores.reconcavo.org.br>
2016-06-29 13:08:02 -0300

    Added ReadWriteFile code

--- app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
+++ app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.whatever.test.androiddemos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        writeStringToFile("String saved to file");
+
+        TextView tv = (TextView) findViewById(R.id.msg_text_view);
+
+        tv.setText(readStringFromFile());
+    }
+
+    private void writeStringToFile(String s){
+        File path = this.getFilesDir();
+        File local_file = new File(path, "local_file.txt");
+
+        if (!local_file.exists()){
+            try {
+                local_file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            FileOutputStream fileOutput = new FileOutputStream(local_file);
+            fileOutput.write(s.getBytes());
+            fileOutput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readStringFromFile(){
+        String s = "";
+
+        File path = this.getFilesDir();
+        File local_file = new File(path, "local_file.txt");
+
+        if (local_file.exists()){
+            try {
+                FileInputStream fileInputStream = new FileInputStream(local_file);
+
+                byte buffer[] = new byte[1024];
+
+                while (fileInputStream.read(buffer) > 0){
+                    s = new String(buffer);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return s;
     }
 }
