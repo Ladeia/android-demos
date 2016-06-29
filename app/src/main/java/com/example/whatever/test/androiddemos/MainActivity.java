776ab90bc180c121fd34ec59b35f85c24a6978b4
Alexandre Amoedo Amorim <alexandre.amorim@consultores.reconcavo.org.br>
2016-06-29 11:47:57 -0300

    Adds IntentBuilder code

--- app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
+++ app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.whatever.test.androiddemos;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void shareMethod(View view) {
+        EditText et = (EditText) findViewById(R.id.msg_edit_text);
+
+        String textToShare = et.getText().toString();
+
+        Intent intent = ShareCompat.IntentBuilder
+                .from(this)
+                .setText(textToShare)
+                .setType("text/plain")
+                .getIntent();
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 }
