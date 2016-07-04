6be6027e3a4a19ff68ad5126d0593383adc9b17a
Alexandre Amoedo Amorim <alexandre.amorim@consultores.reconcavo.org.br>
2016-07-04 17:41:02 -0300

    Adds VideoView code

--- app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
+++ app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.whatever.test.androiddemos;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView mVideoView = (VideoView) findViewById(R.id.video_view);
+
+        Uri uri = Uri.parse("http://techslides.com/demos/sample-videos/small.mp4");
+
+        mVideoView.setVideoURI(uri);
+
+        mVideoView.start();
     }
 }
